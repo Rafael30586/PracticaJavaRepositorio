4c605a360d58f347af4231ca2983f667ccb48703
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-06 00:23:11 -0300

    Herencia e interfaces en java records

--- JavaRecords/src/Personaje.java
+++ JavaRecords/src/Personaje.java
@@ -1,4 +1,7 @@
-public record Personaje(String nombre, String origen, int edad) {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public record Personaje(String nombre, String origen, int edad) implements ActionListener {
 
     public int duplicarEdad(){
         return 2*edad;
@@ -7,4 +10,12 @@ public record Personaje(String nombre, String origen, int edad) {
     public String ponerNombreEnMayuscula(){
         return nombre.toUpperCase();
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    //Segun esta prueba los java records pueden implementar interfaces pero no pueden heredar
+    //de otras clases ni de otros records
 }
