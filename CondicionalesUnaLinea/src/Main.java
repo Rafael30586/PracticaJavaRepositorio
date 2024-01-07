e8403274c0b5a9fe94237cdcc65145c4a525ba09
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-06 23:49:04 -0300

    Practica exitosa con condicionales en una sola linea

---
+++ CondicionalesUnaLinea/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean planeta;
+        String cadena="";
+        int x = 56;
+        int y = 87;
+
+        cadena = (y>x)?"Bien":"Mal";
+        System.out.println("Salio "+cadena);
+        //System.out.println("Hello world!");
+    }
+}
