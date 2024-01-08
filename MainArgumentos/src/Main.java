c5e5b34ceaf53f975450b1857ee77ca90c8d4a0f
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-08 00:38:47 -0300

    Impresion en consola de array vacio

---
+++ MainArgumentos/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //Podria llenar el array desde la terminal asi: java Main String1 String2 String3 ,etc
+        System.out.println(args);
+
+        //System.out.println("Hello world!");
+    }
+}
