4f19a5f706acfab9f82b241036d284acdf340598
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-15 10:52:03 -0300

    Contructor de clase static encapsulado

--- ClasesStatic/src/Consola.java
+++ ClasesStatic/src/Consola.java
@@ -1,6 +1,44 @@
 public class Consola {
 
-    static class Control{
+    private Control control;
 
+    private static class Control{
+        int botonesCantidad;
+        String colorEstandar;
+
+        private Control(int botonesCantidad, String colorEstandar) {
+            this.botonesCantidad = botonesCantidad;
+            this.colorEstandar = colorEstandar;
+        }
+
+        public int getBotonesCantidad() {
+            return botonesCantidad;
+        }
+
+        public void setBotonesCantidad(int botonesCantidad) {
+            this.botonesCantidad = botonesCantidad;
+        }
+
+        public String getColorEstandar() {
+            return colorEstandar;
+        }
+
+        public void setColorEstandar(String colorEstandar) {
+            this.colorEstandar = colorEstandar;
+        }
+    }
+
+    public Control crearControl(int botonesCantidad, String colorEstandar){
+
+        if(this.control==null){
+            return new Control(botonesCantidad,colorEstandar);
+        }else{
+            return this.control;
+        }
+
+    }
+
+    public Control getControl() {
+        return control;
     }
 }
--- ClasesStatic/src/Main.java
+++ ClasesStatic/src/Main.java
@@ -2,12 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         VideoJuego.Personaje personaje = new VideoJuego.Personaje();
-        Consola.Control control = new Consola.Control();
+        VideoJuego.Personaje personaje1 = new VideoJuego.Personaje();
 
         personaje.setNombre("Sonic");
+        personaje1.setNombre("Mario");
 
         System.out.println(personaje.getNombre());
+        System.out.println(personaje1.getNombre());
 
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
     }
 }
