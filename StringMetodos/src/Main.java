95f06d2ab0dabd9baf4b7c1f02a9f01088762cc3
F. Rafael Alvarez <fraq86@gmail.com>
2024-06-12 19:59:56 -0300

    Metodos de clase String

---
+++ StringMetodos/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String cadena = "Halo 3";
+
+        System.out.println("charAt: "+cadena.charAt(2));
+        System.out.println("compareTo: "+cadena.compareTo("Halo 3"));
+        System.out.println("concat: "+cadena.concat(" remake"));
+        System.out.println("contains: "+cadena.contains("lo 3"));
+
+        //System.out.println("Hello world!");
+    }
+}
